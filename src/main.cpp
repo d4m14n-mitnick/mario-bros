73e3edea7e69366391d912aecfcb51f7fd44b436
d4m14n-mitnick <a23310296@live.ceti.mx>
2024-10-23 00:17:00 +0000

    Clase 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,19 @@
-#include <iostream>
-#include <Porcentaje.hpp>
-#include <list>
+#include <SerieNavidad.hpp>
 
-using namespace std;
+int main(int argc, char const *argv[]){
+    
+    SerieNavidad s1(10);
+    SerieNavidad s2(5);
+    SerieNavidad s3(15);
+    
+    s1.Encender();
+    s1.Imprimir();
 
-int main(){
+    s2.Encender();
+    s2.Imprimir();
 
-    std::list<Porcentaje> focos;
+    s3.Encender();
+    s3.Imprimir();
 
-    Porcentaje foco1;
-    Porcentaje foco2;
-    Porcentaje foco3;
-
-    focos.emplace_back(foco1);
-    focos.emplace_back(foco2);
-    focos.emplace_back(foco3);
-
-    for (auto &&f : focos){
-        f.Incrementar(10);
-    }
-
-    foco1.Incrementar(10);
-    foco1.Incrementar(10);
-    foco1.Disminuir(4);
-
-    std::cout
-    <<"Foco 1: "
-    <<foco1.Imprimir()
-    <<std::endl;
-
+    return 0;
 }
